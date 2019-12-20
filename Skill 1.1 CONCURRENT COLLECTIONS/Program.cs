e4dcb022fab12033a5a20f5640f6b4f863b493ee
1008159 <1008159@DES0020>
2019-12-20 09:54:16 -0300

    commit: end Concurrent collections.

--- Skill 1.1 CONCURRENT COLLECTIONS/Program.cs
+++ Skill 1.1 CONCURRENT COLLECTIONS/Program.cs
@@ -43,6 +43,31 @@ namespace Skill_1._1_CONCURRENT_COLLECTIONS
                 Console.WriteLine(result);
             if (bag.TryPeek(out result))
                 Console.WriteLine("There is a next item: { 0}", result);
+
+            // Concurrent Dictionary
+            ConcurrentDictionary<string, int> ages = new ConcurrentDictionary<string, int>();
+
+            if (ages.TryAdd("Rafael", 21)) {
+                Console.WriteLine("Rafael added successfuly.");
+            }
+
+            Console.WriteLine($"Rafael's age {ages["Rafael"]} ");
+
+            Console.ReadKey();
+
+            // set Rafael's age to 22 if it 21
+            if (ages.TryUpdate("Rafael", 22, 21)) {
+                Console.WriteLine("Age updated sucessfully.");
+            }
+            Console.WriteLine($"Rafael's new age {ages["Rafael"]} ");
+
+            Console.ReadKey();
+
+            // increment Rafael's age atomically using factor method.
+            Console.WriteLine($"Rafael's age updated to: {ages.AddOrUpdate("Rafael", 1, (name, age) => age = age + 1)} ");
+            Console.WriteLine($"Rafael's new age {ages["Rafael"]} ");
+
+            Console.ReadKey();
         }
     }
 }
