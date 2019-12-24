495c7e2e50e1f1b58474624aac477c760cfb70b5
1008159 <1008159@DES0020>
2019-12-24 12:58:42 -0300

    commit: manage multithreading.

---
+++ Skill 1.2 Manage Multithreading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill_1._2_Manage_Multithreading
+{
+    class Program
+    {
+        static long sharedTotal;
+
+        static int[] items = Enumerable.Range(0, 500001).ToArray();
+
+        static object sharedTotalLock = new object();
+
+        static void addRangeOfValues(int start, int end) {
+            while (start < end) {
+                lock (sharedTotalLock) {
+                    sharedTotal = sharedTotal + items[start];
+                }
+                start++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            int rangeSize = 1000;
+            int rangeStart = 0;
+
+            while (rangeStart < items.Length) {
+                int rangeEnd = rangeStart + rangeSize;
+
+                if (rangeSize > items.Length) {
+                    rangeEnd = items.Length;
+                }
+
+                // creates local copies of a parameters
+                int rs = rangeStart;
+                int re = rangeEnd;
+
+                tasks.Add(Task.Run(() => addRangeOfValues(rs, re)));
+                rangeStart = rangeEnd;
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"The total is: {sharedTotal}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Skill 1.2 Manage Multithreading/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Skill 1.2 Manage Multithreading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Empreendimentos Pague Menos SA")]
+[assembly: AssemblyProduct("Skill 1.2 Manage Multithreading")]
+[assembly: AssemblyCopyright("Copyright © Empreendimentos Pague Menos SA 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35497431-e886-48c8-982a-5af34d15c685")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
