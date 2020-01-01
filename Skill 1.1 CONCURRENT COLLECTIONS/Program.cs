e96374ece049113ba20f8284a77b4c2b60ef6386
PICHAU <rafaeel.ce@gmail.vom>
2020-01-01 09:07:19 -0300

    commit pull for desktop

--- Skill 1.1 CONCURRENT COLLECTIONS/Program.cs
+++ Skill 1.1 CONCURRENT COLLECTIONS/Program.cs
@@ -11,38 +11,77 @@ namespace Skill_1._1_CONCURRENT_COLLECTIONS
     {
         static void Main(string[] args)
         {
-            // BlockingCollection
-            BlockingCollection<string> col = new BlockingCollection<string>();
+            // Blocking collection that can hold 5 items
+            BlockingCollection<int> data = new BlockingCollection<int>(5);
 
-            Task read = Task.Run(() =>
-            {
-                while (true)
-                {
-                    Console.WriteLine(col.Take());
+            Task.Run(() => {
+                
+                // attempt to add 10 items to he collection - blocks after 5th
+                for (int i = 0; i < 11 ; i++) {
+
+                    data.Add(i);
+                    Console.WriteLine("Data {0} added sucessfully.", i);
+
                 }
+
+                //indicate we have no more to add
+                data.CompleteAdding();
             });
 
-            Task write = Task.Run(() =>
-            {
-                while (true)
-                {
-                    string s = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(s)) break;
-                    col.Add(s);
+            Console.ReadKey();
+            Console.WriteLine("Reading collection");
+
+            Task.Run(() => {
+
+                while (!data.IsCompleted) {
+                    try {
+                        
+                        int v = data.Take();
+                        Console.WriteLine("Data {0} taken sucessfully", v);
+
+                    } catch (InvalidOperationException) { }
                 }
+
             });
 
-            write.Wait();
+            Console.ReadKey();
 
-            // ConcurrentBag
-            ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            bag.Add(42);
-            bag.Add(21);
-            int result;
-            if (bag.TryTake(out result))
-                Console.WriteLine(result);
-            if (bag.TryPeek(out result))
-                Console.WriteLine("There is a next item: { 0}", result);
+            // Concurrent Queue
+            ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+            
+            queue.Enqueue("Rafael");
+            queue.Enqueue("Cruz");
+
+            string str;
+
+            if (queue.TryPeek(out str)) {
+                Console.WriteLine($"Peek: {str}");
+            }
+
+            if (queue.TryDequeue(out str)) {
+                Console.WriteLine($"Dequeue: {str}");
+            }
+
+            Console.ReadKey();
+
+            // Concurrent Stack
+            ConcurrentStack<string> stack = new ConcurrentStack<string>();
+
+            stack.Push("Rafael");
+            stack.Push("Cruz");
+
+            if (stack.TryPeek(out str)) {
+                Console.WriteLine($"Peek: {str} ");
+            }
+
+            if (stack.TryPop(out str))
+            {
+                Console.WriteLine($"Pop: {str} ");
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }
