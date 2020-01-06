c07ea8f8d330e9f604377051a92e7ee607a1f700
PICHAU <rafaeel.ce@gmail.vom>
2020-01-06 18:12:16 -0300

    commit: pch

--- Skill 1.2 Manage Multithreading/Program.cs
+++ Skill 1.2 Manage Multithreading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Skill_1._2_Manage_Multithreading
@@ -11,12 +12,34 @@ namespace Skill_1._2_Manage_Multithreading
         static long sharedTotal;
 
         static int[] items = Enumerable.Range(0, 500001).ToArray();
-
         static object sharedTotalLock = new object();
 
-        static void addRangeOfValues(int start, int end) {
-            while (start < end) {
-                lock (sharedTotalLock) {
+        /// <summary>
+        /// This method work with Monitors for executer.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        static void addRangeOfValuesWithMonitor(int start, int end)
+        {
+            long subTotal = 0;
+
+            while (start < end)
+            {
+                subTotal = subTotal + items[start];
+                start++;
+            }
+
+            Monitor.Enter(sharedTotalLock);
+            sharedTotal = sharedTotal + subTotal;
+            Monitor.Exit(sharedTotalLock);
+        }
+
+        static void addRangeOfValuesWithLock(int start, int end)
+        {
+            while (start < end)
+            {
+                lock (sharedTotalLock)
+                {
                     sharedTotal = sharedTotal + items[start];
                 }
                 start++;
@@ -30,10 +53,12 @@ namespace Skill_1._2_Manage_Multithreading
             int rangeSize = 1000;
             int rangeStart = 0;
 
-            while (rangeStart < items.Length) {
+            while (rangeStart < items.Length)
+            {
                 int rangeEnd = rangeStart + rangeSize;
 
-                if (rangeSize > items.Length) {
+                if (rangeSize > items.Length)
+                {
                     rangeEnd = items.Length;
                 }
 
@@ -41,7 +66,7 @@ namespace Skill_1._2_Manage_Multithreading
                 int rs = rangeStart;
                 int re = rangeEnd;
 
-                tasks.Add(Task.Run(() => addRangeOfValues(rs, re)));
+                tasks.Add(Task.Run(() => addRangeOfValuesWithLock(rs, re)));
                 rangeStart = rangeEnd;
             }
 
