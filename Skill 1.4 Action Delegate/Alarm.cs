33e2ba41f20aa1abdda62014099202926e80246e
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-09 09:50:08 -0300

    commit: Action delegate.

---
+++ Skill 1.4 Action Delegate/Alarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Skill_1._4_Action_Delegate
+{
+    class Alarm
+    {
+        public Action OnAlarmeRaised { get; set; }
+
+        public void RaiseAlarm() {
+            if (OnAlarmeRaised != null) {
+                OnAlarmeRaised();
+            }
+        }
+    }
+}
---
+++ Skill 1.4 Action Delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Skill_1._4_Action_Delegate
+{
+    class Program
+    {
+        static void AlarmListener1() {
+            Console.WriteLine("Alarm listener 1 called.");
+        }
+
+        static void AlarmListener2()
+        {
+            Console.WriteLine("Alarm listener 2 called.");
+        }
+
+        static void Main(string[] args)
+        {
+            //create a new alarm
+            Alarm alarm = new Alarm();
+
+            //connect the two listener methods
+            alarm.OnAlarmeRaised += AlarmListener1;
+            alarm.OnAlarmeRaised += AlarmListener2;
+
+            //raise the alarm
+            alarm.RaiseAlarm();
+            Console.WriteLine("Alarm raised");
+
+            Console.ReadKey();
+
+        }
+    }
+}
