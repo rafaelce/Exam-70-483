54e0633859703686fb656bc4abce8649f818e464
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-18 07:37:53 -0300

    commit: example of extension methods.

---
+++ Extension Methods/MyExtensionsMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    public static class MyExtensionsMethods
+    {
+        public static int LineCount(this String str)
+        {
+            return str.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
