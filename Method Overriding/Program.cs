368ad7cff707da5cba22e0cc5e8e24d77bd5a0ff
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-19 09:36:15 -0300

    commit: method overriding.

---
+++ Method Overriding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    class Program
+    {
+        class Document
+        {
+            public void GetDate()
+            {
+                Console.WriteLine("Hello from GetDate in Document");
+            }
+
+            //only methods that have been marked as 
+            //virtual in the parent class can be overridden
+            public virtual void DoPrint()
+            {
+                Console.WriteLine("Hello from DoPrint in Document");
+            }
+
+        }
+
+        class Invoice : Document
+        {
+            public override void DoPrint()
+            {
+                Console.WriteLine("Hello from DoPrint in Invoice");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Invoice c = new Invoice();
+
+            c.GetDate();
+
+            c.DoPrint();
+
+            Console.ReadKey();
+        }
+    }
+}
