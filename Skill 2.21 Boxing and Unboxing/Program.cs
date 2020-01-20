f36b0a95bc59d3530ec65397ed94edafc0fb67ad
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-20 20:47:19 -0300

    commit: cast and convert types

---
+++ Skill 2.21 Boxing and Unboxing/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill_2._21_Boxing_and_Unboxing
+{
+    class Program
+    {
+        class Miles
+        {
+            public Double Distance { get; }
+
+            public static implicit operator Kilometers(Miles t)
+            {
+                Console.WriteLine("Implicite conversion from miles to kilometers");
+                return new Kilometers(t.Distance * 1.6);
+            }
+
+            public static explicit operator int(Miles t)
+            {
+                Console.WriteLine("Implicite conversion from miles to int");
+                return (int)(t.Distance + 0.5);
+            }
+
+            public Miles(double miles)
+            {
+                Distance = miles;
+            }
+        }
+
+        class Kilometers
+        {
+            public Double Distance { get; }
+
+            public Kilometers(double kilimeters)
+            {
+                Distance = kilimeters;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // thw value 99 is boxed into a object
+            object o = 99;
+
+            // the boxed object is unboxed back into an int
+            int oVal = (int)o;
+            Console.WriteLine($"The value is {oVal} \n\n");
+
+
+            Miles m = new Miles(100);
+
+            Kilometers k = m; //implicity convert miles to km.
+            Console.WriteLine($"Kilometers: {k.Distance} ");
+
+            int intMiles = (int)m; // explicity convert miles to int.
+            Console.WriteLine($"Int miles: {intMiles} ");
+
+            Console.ReadKey();
+        }
+    }
+}
