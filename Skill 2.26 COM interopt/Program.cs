2795ef35dee3e5b3274752302786ac28b1b20f54
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-23 16:18:44 -0300

    commit: using a property.

--- Skill 2.26 COM interopt/Program.cs
+++ Skill 2.26 COM interopt/Program.cs
@@ -9,32 +9,47 @@ namespace Skill_2._26_COM_interopt
     class Program
     {
         class Customer {
-            public string Name;
+            public string Nome;
+            private string _nameValue;
+
+            public string PrivateName {
+                get {
+                    return _nameValue;
+                }
+                set {
+                    if (value == "")
+                        throw new Exception("Invalid customer name.");
+                    _nameValue = value;
+                }
+            }
         }
 
         static void Main(string[] args)
         {
             // Create the interop 
-            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            //var excelApp = new Microsoft.Office.Interop.Excel.Application();
 
             // make the app visible 
-            excelApp.Visible = true;
+            //excelApp.Visible = true;
 
             // Add a new workbook 
-            excelApp.Workbooks.Add();
+            //excelApp.Workbooks.Add();
 
             // Obtain the active sheet from the app 
             // There is no need to cast this dynamic type 
-            Microsoft.Office.Interop.Excel.Worksheet workSheet = excelApp.ActiveSheet;
+            //Microsoft.Office.Interop.Excel.Worksheet workSheet = excelApp.ActiveSheet;
 
             // Write into two cells 
-            workSheet.Cells[ 1, "A"] = "Hello";
-            workSheet.Cells[ 1, "B"] = "from C#";
+            //workSheet.Cells[ 1, "A"] = "Hello";
+            //workSheet.Cells[ 1, "B"] = "from C#";
 
             Customer c = new Customer();
-            c.Name = "Rafael";
+            c.Nome = "Rafael";
 
-            Console.WriteLine($"Customer name: {c.Name}");
+            Console.WriteLine($"Customer name: {c.Nome}");
+
+            c.PrivateName = "Rafael Cruz";
+            Console.WriteLine($"Customer name: {c.PrivateName}");
 
             Console.ReadKey();
         }
