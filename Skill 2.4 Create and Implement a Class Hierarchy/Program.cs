7e7e1c555770f964e6b1611b88f5d998fd8ee6e2
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-30 12:47:45 -0300

    commit: implementation a interface.

--- Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
+++ Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
@@ -8,8 +8,28 @@ namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
 {
     class Program
     {
+        interface IPrintable
+        {
+            string GetPrintableText(int pageWidth, int pageHeight);
+            string GetTitle();
+        }
+
+        class Report : IPrintable
+        {
+            public string GetPrintableText(int pageWidth, int pageHeight)
+            {
+                return "Report Text";
+            }
+
+            public string GetTitle()
+            {
+                return "Report Title";
+            }
+        }
         static void Main(string[] args)
         {
         }
+
+
     }
 }
