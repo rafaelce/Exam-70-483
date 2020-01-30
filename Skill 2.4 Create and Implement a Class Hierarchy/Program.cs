0d8838c682ef29af326f692296137f0d5b3428ac
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-30 12:43:58 -0300

    commit: Create and implement a Class Hierarchy

---
+++ Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Skill 2.4 Create and Implement a Class Hierarchy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Skill 2.4 Create and Implement a Class Hierarchy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Empreendimentos Pague Menos SA")]
+[assembly: AssemblyProduct("Skill 2.4 Create and Implement a Class Hierarchy")]
+[assembly: AssemblyCopyright("Copyright © Empreendimentos Pague Menos SA 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08515c55-151e-49f4-8a8a-c9125f35bcc9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
