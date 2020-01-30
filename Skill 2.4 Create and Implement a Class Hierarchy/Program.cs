ced8ec337ecf152db99a1467227ba06acc582bf2
Rafael Cruz <rafaeel.ce@gmail.com>
2020-01-30 13:49:46 -0300

    commit: implementation IAccount interdade with explicitly implemented.

--- Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
+++ Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
@@ -14,6 +14,36 @@ namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
             string GetTitle();
         }
 
+        interface IAccount
+        {
+            void PayInFunds(decimal amount);
+            bool WithdrawFunds(decimal amount);
+            decimal GetBalance();
+        }
+
+        public class BankAccount : IAccount
+        {
+            private decimal _balance = 0;
+            decimal IAccount.GetBalance()
+            {
+                return _balance;
+            }
+
+            void IAccount.PayInFunds(decimal amount)
+            {
+                _balance = _balance + amount;
+            }
+
+            bool IAccount.WithdrawFunds(decimal amount)
+            {
+                if (_balance < amount)
+                    return false;
+
+                _balance = _balance - amount;
+                return true;
+            }
+        }
+
         class Report : IPrintable
         {
             public string GetPrintableText(int pageWidth, int pageHeight)
