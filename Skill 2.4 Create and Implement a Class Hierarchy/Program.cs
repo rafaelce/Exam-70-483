14e091d7da348e47ffb81b21097344570abca609
Rafael Cruz <rafaeel.ce@gmail.com>
2020-02-03 14:22:01 -0300

    commit: before IDisposable

--- Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
+++ Skill 2.4 Create and Implement a Class Hierarchy/Program.cs
@@ -15,9 +15,15 @@ namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
             decimal GetBalance();
         }
 
-        public class BankAccount : IAccount
+        public class BankAccount : IAccount, IComparable
         {
             private decimal _balance = 0;
+
+            public BankAccount(decimal initialBalance)
+            {
+                _balance = initialBalance;
+            }
+
             decimal IAccount.GetBalance()
             {
                 return _balance;
@@ -36,19 +42,35 @@ namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
                 _balance = _balance - amount;
                 return true;
             }
+
+            //public abstract string WarningLetterString();
+
+            public int CompareTo(object obj)
+            {
+                if (obj == null) return 1;
+
+                IAccount account = obj as IAccount;
+
+                if (account == null)
+                    throw new ArgumentException("Object is not an account");
+
+                return this._balance.CompareTo(account.GetBalance());
+            }
         }
 
         public class BabyAccount : BankAccount, IAccount
         {
+            public BabyAccount(int initialBalance) : base(initialBalance)
+            {
+            }
+
             //public override bool WithdrawFunds(decimal amount)
             //{
-            //    if (amount > 10)
-            //    {
+            //    if (amount > 10){
             //        return false;
             //    }
 
-            //    if (_balance < amount)
-            //    {
+            //    if (_balance < amount){
             //        return false;
             //    }
 
@@ -90,19 +112,26 @@ namespace Skill_2._4_Create_and_Implement_a_Class_Hierarchy
         }
         static void Main(string[] args)
         {
-            IAccount account = new BankAccount();
-            account.PayInFunds(50);
+            //IAccount account = new BankAccount();
+            //account.PayInFunds(50);
 
-            Console.WriteLine($"Balance: {account.GetBalance()}");
+            //Console.WriteLine($"Balance: {account.GetBalance()}");
 
-            IAccount baby = new BabyAccount();
-            baby.PayInFunds(50);
+            //IAccount baby = new BabyAccount();
+            //baby.PayInFunds(50);
 
-            if (baby is IAccount)
-                Console.WriteLine("\nthis object can be used as an account.");
-            else
-                Console.WriteLine("\nthis object cannot be used as an account.");
+            //if (baby is IAccount)
+            //    Console.WriteLine("\nthis object can be used as an account.");
+            //else
+            //    Console.WriteLine("\nthis object cannot be used as an account.");
 
+
+            var name = "Rafael Cruz Ferreira".GetEnumerator();
+
+            while (name.MoveNext())
+            {
+                Console.WriteLine(name.Current);
+            }
 
             Console.ReadKey();
         }
