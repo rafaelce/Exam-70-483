d9da7c74bffb213c81f5daa04876c385dff3752b
Rafael Cruz <rafaeel.ce@gmail.com>
2020-02-06 14:25:06 -0300

    commit: the serializable attribute.

---
+++ Skill 2.5 Reflection/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill_2._5_Reflection
+{
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [NonSerialized]
+        private int screenPosition;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            screenPosition = 0;
+        }
+    }
+}
