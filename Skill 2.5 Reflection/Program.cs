b8d2d5b0e214899cbebe1595caabbf1270b39ca0
Rafael Cruz <rafaeel.ce@gmail.com>
2020-02-10 09:41:17 -0300

    commit: GetType

--- Skill 2.5 Reflection/Program.cs
+++ Skill 2.5 Reflection/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿#define TERSE
+//#define VERBOSE
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +13,48 @@ namespace Skill_2._5_Reflection
 {
     class Program
     {
+        [Conditional("VERBOSE"), Conditional("TERSE")]
+        static void reportHeader()
+        {
+            Console.WriteLine("This is the header for the report.");
+        }
+
+        [Conditional("VERBOSE")]
+        static void verboseReport()
+        {
+            Console.WriteLine("This is output from the verbose report.");
+        }
+
+        [Conditional("TERSE")]
+        static void terseReport()
+        {
+            Console.WriteLine("This is output from the terse report.");
+        }
+
+        class Pessoa
+        {
+            public string Name { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            reportHeader();
+            terseReport();
+            verboseReport();
+
+            System.Type type;
+
+            Pessoa p = new Pessoa();
+            type = p.GetType();
+
+            foreach (MemberInfo member in type.GetMembers())
+            {
+                Console.WriteLine($"{member.ToString()} \n");
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
